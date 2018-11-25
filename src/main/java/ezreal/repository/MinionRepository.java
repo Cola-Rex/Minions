7225ac2fbdcd06c0e6c53aa7099e7897adc04f1d
Ezreal <1649811141@qq.com>
2018-11-25 23:32:48 +0800

    Create MinionRepository.java

---
+++ src/main/java/ezreal/repository/MinionRepository.java
@@ -0,0 +1,9 @@
+package ezreal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ezreal.entity.Minion;
+
+public interface MinionRepository extends JpaRepository<Minion, Long> {
+
+}
