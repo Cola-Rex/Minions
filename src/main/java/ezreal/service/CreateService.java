e749adf6ae8f8e3f3f2a5d90dfdbbb8a46d84a52
Ezreal <1649811141@qq.com>
2018-11-29 16:40:56 +0800

    Update CreateService.java

--- src/main/java/ezreal/service/CreateService.java
+++ src/main/java/ezreal/service/CreateService.java
@@ -23,7 +23,7 @@ public class CreateService {
 	//保存所有容器ID的集合
 	private List<String> containerList = new ArrayList<>();
 	//保存所有eureka 的地址信息
-	private List<URI> uriList = new ArrayList<>();
+	private ArrayList<URI> uriList = new ArrayList<>();
 	
 	public void createEureka() {
 		//获取所有在线的eureka server
@@ -37,13 +37,43 @@ public class CreateService {
 			}
 		}
 		
-		//调用gitlab API 更新配置文件
-		gitService.createFile("eureka-server-peer" + (size++) + ".yml");
+		//创建新的 eureka 配置文件
+		StringBuilder sb = new StringBuilder();
+		if (size == 0) {
+			sb.append("http://localhost:9001/eureka/"); //默认第一个 server 端口 9001
+		} else {
+			//InetAddress addr = InetAddress.getLocalHost();
+			//新建 eureka 的uri
+			for (URI uri : uriList) {
+				if (uri.getPort() != 9001 + size) {
+					sb.append("http://" + uri.getHost() + ":" + uri.getPort() + "/eureka/,");
+				}
+			}
+			sb.deleteCharAt(sb.length() - 1); //删除最后1个逗号
+		}
+		
+		gitService.createFile(size++, sb.toString());
+		
+		//更新其他 eureka 配置
+		if (size > 0) {
+			for (URI uri : uriList) {
+				StringBuilder sb1 = new StringBuilder();
+				@SuppressWarnings("unchecked")
+				List<URI> tempList = (List<URI>) uriList.clone();
+				tempList.remove(uri);
+				for (URI temp : tempList) {
+					sb1.append("http://" + temp.getHost() + ":" + uri.getPort() + "/eureka/,");
+				}
+				sb1.deleteCharAt(sb1.length() - 1);
+				gitService.updateFile(uri.getPort() - 9000, sb1.toString());
+			}
+		}
 		
 		//调用shell命令创建eureka server
 		String containerId = shellService.createEureka(size++);
 		containerList.add(containerId);
 		
+		//TODO 刷新其他服务
 	}
 	
 }
