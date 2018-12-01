49acc7e7c02035e368e735a1c50997921d259953
Ezreal <1649811141@qq.com>
2018-12-01 13:17:27 +0800

    Update SpringMinionApplication.java

--- src/main/java/ezreal/SpringMinionApplication.java
+++ src/main/java/ezreal/SpringMinionApplication.java
@@ -2,12 +2,14 @@ package ezreal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * 主函数
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients
 public class SpringMinionApplication {
 
