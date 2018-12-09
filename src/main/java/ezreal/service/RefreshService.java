7624d3d57d07081385833c995facf34c42d11d42
Ezreal <1649811141@qq.com>
2018-12-09 22:49:49 +0800

    Update RefreshService.java
    更新“重启其他服务的请求”

--- src/main/java/ezreal/service/RefreshService.java
+++ src/main/java/ezreal/service/RefreshService.java
@@ -1,23 +1,24 @@
 package ezreal.service;
 
-import java.net.URI;
-import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import ezreal.entity.Minion;
+
 @Service
 public class RefreshService {
 
 	private static final Logger log = LoggerFactory.getLogger(RefreshService.class);
 	
-	public void refresh(ArrayList<URI> uriList) {
+	public void refresh(List<Minion> list) {
 		RestTemplate restTemplate = new RestTemplate();
-		for (URI uri : uriList) {
-			restTemplate.postForObject("http://" + uri.getHost() + ":" + uri.getPort() + "/refresh", null, String.class);
-			log.info("服务 {}:{} 更新完毕");
+		for (Minion minion : list) {
+			restTemplate.postForObject("http://" + minion.getAddress() + "/refresh", null, String.class);
+			log.info("服务 {} 更新完毕", minion.getAddress());
 		}
 	}
 }
